8e8599f149bae9733ff32125fc7240ac91053fed
zhongyue.zhao <zhongyue.zhao@seecture.com>
2024-07-10 17:15:19 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+    <button class="btn" @click="add">{{ state.data.num }}</button>
+  </div>
+</template>
+<script setup>
+import { reactive } from 'vue';
+
+
+const state = reactive({
+  data: {
+    num: 0
+  }
+})
+
+
+const add = () => {
+  state.data.num++
+
+}
+
+</script>
+
+<style scoped>
+div{
+  display: flex;
+  flex-direction: column;
+}
+.btn{
+}
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
